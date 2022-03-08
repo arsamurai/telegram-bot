c595d1797893359d0895bdc057af435a97d42254
Arsamurai <musienkoartur45@gmail.com>
2022-03-08 11:27:53 +0200

    add textincludes

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const TelegramBot = require("node-telegram-bot-api");
 const token = "5222945979:AAGs9GShgnXD0P0S5yuPZIscm6QeNsE-OdM";
-const { gameOptions, againOptions, unluckyDaysOptions, jokesOptions } = require("./options");
+const {
+  gameOptions,
+  againOptions,
+  unluckyDaysOptions,
+  jokesOptions,
+} = require("./options");
 const { black_humor, stupid_humor_plus, stupid_humor } = require("./jokes");
 
 const bot = new TelegramBot(token, { polling: true });
@@ -12,22 +17,33 @@ let usedBlackHumor = [];
 let usedStupidHumor = [];
 let usedStupidHumorPlus = [];
 
+let isKeyWord = false;
+
 const startGame = async (chatId, name) => {
   await bot.sendMessage(chatId, `${name}, я загадал решку или орла`);
-  const rememberNum = Math.floor( 1 + Math.random() * 2);
+  const rememberNum = Math.floor(1 + Math.random() * 2);
   chats[chatId] = rememberNum;
   await bot.sendMessage(chatId, "Твоя задача угадать...", gameOptions);
 };
 
 const getJoke = (usedAlready, jokes) => {
-	let numOfJoke = Math.floor(Math.random() * jokes.length);
-	if(!usedAlready.includes(jokes[numOfJoke])) {
-		usedAlready.push(jokes[numOfJoke]);
-		return numOfJoke;
-	} else {
-		return getJoke(usedAlready, jokes);
+  let numOfJoke = Math.floor(Math.random() * jokes.length);
+  if (!usedAlready.includes(jokes[numOfJoke])) {
+    usedAlready.push(jokes[numOfJoke]);
+    return numOfJoke;
+  } else {
+    return getJoke(usedAlready, jokes);
+  }
+};
+
+const textIncludes = (text, chatId, words, message) => {
+	for (let i = 0; i < words.length; i++) {
+		if(text.includes(words[i])) {
+			isKeyWord = true;
+			return bot.sendMessage(chatId, `${message}`);
+		}
 	}
-}
+};
 
 const start = () => {
   bot.setMyCommands([
@@ -38,34 +54,38 @@ const start = () => {
       command: "/unluckydays",
       description: "Count unlucky days in some year",
     },
-		{ command: "/jokes", description: "Best humor in the world" },
+    { command: "/jokes", description: "Best humor in the world" },
   ]);
 
   bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
     const text = msg && msg.text?.toLowerCase();
+    const sticker = msg && msg.sticker && msg.sticker;
+    const animation = msg && msg.animation && msg.animation;
     const name = msg.from.first_name;
+		isKeyWord = false;
 
-		if (text.includes("шутк")) {
-			const jokes = [...black_humor, ...stupid_humor, ...stupid_humor_plus];
-			if(usedJokes.length > 24) {
-				usedJokes = [];
-				await bot.sendMessage(chatId, "Упс, шутки закончились, буду повторять, неугомонний, бля)");
-			} else await bot.sendMessage(chatId, `Хтось хоче шутку?`);
-			const joke = jokes[getJoke(usedJokes, jokes)];
-			return bot.sendMessage(chatId, joke);
+    if (sticker || animation) {
+      return bot.sendMessage(chatId, "Заебок");
     }
-		if (text.includes('ах') ||
-				text.includes('хa') ||
-				text.includes('пх')) {
-      return bot.sendMessage(chatId, `Ага, сам чуть не впісявся`);
+    if (text.includes("шутк")) {
+      const jokes = [...black_humor, ...stupid_humor, ...stupid_humor_plus];
+      if (usedJokes.length > 24) {
+        usedJokes = [];
+        await bot.sendMessage(
+          chatId,
+          "Упс, шутки закончились, буду повторять, неугомонний, бля)"
+        );
+      } else await bot.sendMessage(chatId, `Хтось хоче шутку?`);
+      const joke = jokes[getJoke(usedJokes, jokes)];
+      return bot.sendMessage(chatId, joke);
     }
-		if (text.includes("слава україні")) {
-      return bot.sendMessage(chatId, `Героям Слава!`);
-    }
-		if (text === "cпасибо" || text === "дякую") {
-      return bot.sendMessage(chatId, `Та йди нахер)`);
-    }
+		
+		textIncludes(text, chatId, ["аха", "хaх", "пхп"], "Ага, сам чуть не впісявся");
+		textIncludes(text, chatId, ["дякую"], "Та йди нахер)");
+		textIncludes(text, chatId, ["слава україні"], "Героям Слава!");
+		textIncludes(text, chatId, ["дякую"], "Та йди нахер)");
+		
     if (text === "/start") {
       return bot.sendMessage(chatId, `${name}, здарова`);
     }
@@ -82,7 +102,7 @@ const start = () => {
       return bot.sendMessage(chatId, "Захотелось поугарать?...", jokesOptions);
     }
 
-    return bot.sendMessage(chatId, "Я хз, шо ти хочеш(");
+    !isKeyWord && bot.sendMessage(chatId, "Я хз, шо ти хочеш(");
   });
 
   bot.on("callback_query", async (msg) => {
@@ -90,34 +110,35 @@ const start = () => {
     const chatId = msg.message.chat.id;
     const name = msg.from.first_name;
 
-    if(data === "/again") {
+    if (data === "/again") {
       return startGame(chatId, name);
     }
-		if(data === "black-humor") {
-			if(usedBlackHumor.length > 8) {
-				usedBlackHumor = [];
-				await bot.sendMessage(chatId, "Упс, шутки закончились, на повтор!");
-			}
-			const joke = black_humor[getJoke(usedBlackHumor, black_humor)];
-			return bot.sendMessage(chatId, joke);
-		}
-		if(data === "stupid-humor") {
-			if(usedStupidHumor.length > 10) {
-				usedStupidHumor = [];
-				await bot.sendMessage(chatId, "Упс, шутки закончились, на повтор!");
-			}
-			const joke = stupid_humor[getJoke(usedStupidHumor, stupid_humor)];
-			return bot.sendMessage(chatId, joke);
-		}
-		if(data === "stupid-humor-plus") {
-			if(usedStupidHumorPlus.length > 4) {
-				usedStupidHumorPlus = [];
-				await bot.sendMessage(chatId, "Упс, шутки закончились, на повтор!");
-			}
-			const joke = stupid_humor_plus[getJoke(usedStupidHumorPlus, stupid_humor_plus)];
-			return bot.sendMessage(chatId, joke);
-		}
-		if(data.length === 4) {
+    if (data === "black-humor") {
+      if (usedBlackHumor.length > 8) {
+        usedBlackHumor = [];
+        await bot.sendMessage(chatId, "Упс, шутки закончились, на повтор!");
+      }
+      const joke = black_humor[getJoke(usedBlackHumor, black_humor)];
+      return bot.sendMessage(chatId, joke);
+    }
+    if (data === "stupid-humor") {
+      if (usedStupidHumor.length > 10) {
+        usedStupidHumor = [];
+        await bot.sendMessage(chatId, "Упс, шутки закончились, на повтор!");
+      }
+      const joke = stupid_humor[getJoke(usedStupidHumor, stupid_humor)];
+      return bot.sendMessage(chatId, joke);
+    }
+    if (data === "stupid-humor-plus") {
+      if (usedStupidHumorPlus.length > 4) {
+        usedStupidHumorPlus = [];
+        await bot.sendMessage(chatId, "Упс, шутки закончились, на повтор!");
+      }
+      const joke =
+        stupid_humor_plus[getJoke(usedStupidHumorPlus, stupid_humor_plus)];
+      return bot.sendMessage(chatId, joke);
+    }
+    if (data.length === 4) {
       let count = 0;
       let date = new Date(data, 0, 1);
 
@@ -129,16 +150,20 @@ const start = () => {
       }
       return bot.sendMessage(chatId, `В ${data}'ом пятниц 13го - ${count}`);
     }
-    if(data == chats[chatId]) {
+    if (data == chats[chatId]) {
       return bot.sendMessage(
         chatId,
-        `Поздравляю, ти угадав ${chats[chatId] === 1 ? 'орла' : 'решку'}, йди набухайся!`,
+        `Поздравляю, ти угадав ${
+          chats[chatId] === 1 ? "орла" : "решку"
+        }, йди набухайся!`,
         againOptions
       );
     } else {
       return bot.sendMessage(
         chatId,
-        `Сорі, ти проэбався( Я загадав ${chats[chatId] === 1 ? 'орла' : 'решку'}`,
+        `Сорі, ти проэбався( Я загадав ${
+          chats[chatId] === 1 ? "орла" : "решку"
+        }`,
         againOptions
       );
     }
